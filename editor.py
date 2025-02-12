823309cdb70281e24c253a2fedf86c3caca9b4f4
Alex <oleksii.mindra@gmail.com>
2025-02-12 20:48:41 +0100

    added interface in editor

--- editor.py
+++ editor.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog
 
 app = QApplication([])
+from widgetsed import *
+
 window = QWidget()
 window.resize(700, 400)
 window.setWindowTitle("Editor")
-
+window.setLayout(hline_main)
 
 
 window.show()
